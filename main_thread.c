81e6c494a0201ef2e5fe9858ee9dabc0039d0f22
Sebastiao <s.jeronimo2002@gmail.com>
2024-03-09 10:26:40 +0000

    added a main to test threads

---
+++ main_thread.c
@@ -0,0 +1,9 @@
+int main(){
+    /*
+    memset, printf, malloc, free, write,
+    usleep, gettimeofday, pthread_create,
+    pthread_detach, pthread_join, pthread_mutex_init,
+    pthread_mutex_destroy, pthread_mutex_lock,
+    pthread_mutex_unlock
+    */
+}
