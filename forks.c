5880613cc5a7e8809c30dbe6e2ec10637540f364
Sebastião <s.jeronimo2002@gmail.com>
2024-03-28 20:10:27 +0000

    just need to implement the get forks and pick forks method and the respective printing

---
+++ forks.c
@@ -0,0 +1,22 @@
+#include "philosophers.h"
+
+/**
+ * @brief picks up the forks for a specific philosopher
+ * if there is 3 philosophers
+ * 1 = (1 , 2)
+ * 2 = (2 , 3)
+ * 3 = (3 , 1)
+ */
+void get_forks()
+{
+    printf(TO_DO);
+}
+
+/**
+ * @brief a specific philosopher drops the forkss
+ * 
+ */
+void drop_forks()
+{
+    printf(TO_DO);
+}
