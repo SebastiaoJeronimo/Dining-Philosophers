0a4d05ec2fb97358edc5e45bdfaec05778fc06d7
Sebastiao <s.jeronimo2002@gmail.com>
2024-03-29 07:15:15 +0000

    delete extra files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Dining-Philosophers
 my resolution of the well known dining philosophers problem see the subject
+ 
--- data.c
+++
@@ -1,7 +0,0 @@
-#include "philosophers.h"
-
-t_data *get_data(){
-    static t_data data;
-    
-    return (&data);
-}
